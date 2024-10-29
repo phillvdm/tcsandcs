2a8a8e9ab94fc6c7407ec4da254559fe705cce9f
phillvdm <phillvdm@gmail.com>
2024-10-29 11:57:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tcsandcs
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/phillvdm/tcsandcs)
---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <div class="title">Terms and conditions:</div>
+    <div class="content">
+      By sharing personal information with Phillip and Alma you agree to give Puzzle pets and an extra treat at dinner time. Good boy, Puzz!
+    </div>
+  </div>
+`
